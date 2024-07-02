da25ff7dfe4b86a6b69e1a4f4f3b4a2dd3eaa109
Justine Bouin <112966252+alt-ju@users.noreply.github.com>
2024-07-02 14:08:48 +0200

    Add comments in controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,8 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+/**
+ * Class dans le controller qui permet de générer du contenu HTML dans le template/la vue, ici home.html.twig, grâce à la fonction render() présente dans AbstractController mis à dispositon par Symfony
+ */
 class HomeController extends AbstractController
 {
+    // Définir la route pour le navigateur
     #[Route('/', 'home.index', methods : ['GET'])]
     public function index(): Response 
     {
